a2ba4ad12d926d10d9869db92a8b7cca2d2bcad0
kartikhegde <kartikshrikanthegde@gmail.com>
2019-07-31 18:57:57 -0500

    added deployment scripts to gcp + travis yml file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t "$DOCKER_USERNAME"/multi-client:latest -t "$DOCKER_USERNAME"/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t "$DOCKER_USERNAME"/multi-server:latest -t "$DOCKER_USERNAME"/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t "$DOCKER_USERNAME"/multi-worker:latest -t "$DOCKER_USERNAME"/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t "$DOCKER_ID"/multi-client:latest -t "$DOCKER_ID"/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t "$DOCKER_ID"/multi-server:latest -t "$DOCKER_ID"/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t "$DOCKER_ID"/multi-worker:latest -t "$DOCKER_ID"/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push "$DOCKER_USERNAME"/multi-client:latest
-docker push "$DOCKER_USERNAME"/multi-server:latest
-docker push "$DOCKER_USERNAME"/multi-worker:latest
+docker push "$DOCKER_ID"/multi-client:latest
+docker push "$DOCKER_ID"/multi-server:latest
+docker push "$DOCKER_ID"/multi-worker:latest
 
-docker push "$DOCKER_USERNAME"/multi-client:$SHA
-docker push "$DOCKER_USERNAME"/multi-server:$SHA
-docker push "$DOCKER_USERNAME"/multi-worker:$SHA
+docker push "$DOCKER_ID"/multi-client:$SHA
+docker push "$DOCKER_ID"/multi-server:$SHA
+docker push "$DOCKER_ID"/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server="$DOCKER_USERNAME"/multi-server:$SHA
-kubectl set image deployments/client-deployment client="$DOCKER_USERNAME"/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker="$DOCKER_USERNAME"/multi-worker:$SHA
+kubectl set image deployments/server-deployment server="$DOCKER_ID"/multi-server:$SHA
+kubectl set image deployments/client-deployment client="$DOCKER_ID"/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker="$DOCKER_ID"/multi-worker:$SHA
