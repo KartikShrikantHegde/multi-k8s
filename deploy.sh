f16dfbd8d68fab05b57bf1ef3e6842648c2c8721
kartikhegde <kartikshrikanthegde@gmail.com>
2019-07-31 18:50:38 -0500

    added deployment scripts to gcp + travis yml file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t karthdz/multi-client:latest -t karthdz/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t karthdz/multi-server:latest -t karthdz/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t karthdz/multi-worker:latest -t karthdz/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t "$DOCKER_USERNAME"/multi-client:latest -t "$DOCKER_USERNAME"/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t "$DOCKER_USERNAME"/multi-server:latest -t "$DOCKER_USERNAME"/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t "$DOCKER_USERNAME"/multi-worker:latest -t "$DOCKER_USERNAME"/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push karthdz/multi-client:latest
-docker push karthdz/multi-server:latest
-docker push karthdz/multi-worker:latest
+docker push "$DOCKER_USERNAME"/multi-client:latest
+docker push "$DOCKER_USERNAME"/multi-server:latest
+docker push "$DOCKER_USERNAME"/multi-worker:latest
 
-docker push karthdz/multi-client:$SHA
-docker push karthdz/multi-server:$SHA
-docker push karthdz/multi-worker:$SHA
+docker push "$DOCKER_USERNAME"/multi-client:$SHA
+docker push "$DOCKER_USERNAME"/multi-server:$SHA
+docker push "$DOCKER_USERNAME"/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=karthdz/multi-server:$SHA
-kubectl set image deployments/client-deployment client=karthdz/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=karthdz/multi-worker:$SHA
+kubectl set image deployments/server-deployment server="$DOCKER_USERNAME"/multi-server:$SHA
+kubectl set image deployments/client-deployment client="$DOCKER_USERNAME"/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker="$DOCKER_USERNAME"/multi-worker:$SHA
